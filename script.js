e28192489fe0dfd01de30453208cc06ff28511ea
Andy Flickinger <atflick@gmail.com>
2017-03-23 11:14:54 -0400

    blah

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
-d3.select("body").transition()
-    .style("background-color", "black");
 
-d3.select("body").append("p").text("New paragraph!");
 
-function bodyBlue() {
-  d3.select("div").style("background-color", "blue")
-  console.log(2 + 2)
-}
+// d3.select("body").append("p").text("New paragraph!");
 
-var svg = d3.select('body')
-  .append('svg')        // create an <svg> element
-    .attr('width', 300) // set its dimentions
-    .attr('height', 150);
+// function bodyBlue() {
+//   d3.select("div").style("background-color", "blue")
+//   console.log(2 + 2)
+// }
+//
+// var svg = d3.select('body')
+//   .append('svg')        // create an <svg> element
+//     .attr('width', 300) // set its dimentions
+//     .attr('height', 150);
+//
+// bodyBlue()
 
-bodyBlue()
-
-console.log(d3)
+// console.log(d3)
