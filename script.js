be4adb5b6e4701cfdb2762de56c58d03e5c3a6e9
Andy Flickinger <atflick@gmail.com>
2017-03-23 10:54:58 -0400

    testing

---
+++ script.js
@@ -0,0 +1,19 @@
+
+d3.select("body").transition()
+    .style("background-color", "black");
+
+d3.select("body").append("p").text("New paragraph!");
+
+function bodyBlue() {
+  d3.select("div").style("background-color", "blue")
+  console.log(2 + 2)
+}
+
+var svg = d3.select('body')
+  .append('svg')        // create an <svg> element
+    .attr('width', 300) // set its dimentions
+    .attr('height', 150);
+
+bodyBlue()
+
+console.log(d3)
